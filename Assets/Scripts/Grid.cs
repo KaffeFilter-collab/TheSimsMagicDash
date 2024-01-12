629fbdf6742d4a9073d176a0060b397a5a88d999
KaffeFilter-collab <148773655+KaffeFilter-collab@users.noreply.github.com>
2024-01-12 13:16:11 +0100

    Gridoptimisation

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,32 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
 
 public class Grid 
 {
-
+    new Vector3 parentPosition;
     private int width;
     private int height;
     private int[,] gridArray;
-    [SerializeField] int CellSize;
-    public Grid(int width,int height) 
-    { 
+    [SerializeField] float CellSize=1;
+    public Grid(int width,int height,float CellSize,Transform parent) 
+    {
+        parentPosition = parent.position;
         this.width = width;
         this.height = height;
         gridArray = new int[width,height];
+        Debug.DrawLine(position(width, 0), position(width,height), Color.white, 100f);
+        Debug.DrawLine(position(0, height), position(width ,height), Color.white, 100f);
         
         for (int x = 0;x< gridArray.GetLength(0);x++)
         {
             for (int y = 0;y< gridArray.GetLength(1);y++)
             {
-                CreateWorldText(gridArray[x, y].ToString(), null ,GetWorldposition(x, y),20, Color.white, TextAnchor.MiddleCenter);
+                CreateWorldText(parent,gridArray[x, y].ToString(),position(x, y),5, Color.white, TextAnchor.MiddleCenter,TextAlignment.Center,1);
+                
+                Debug.DrawLine(position(x, y),position(x,y+1),Color.white, 100f);
+                Debug.DrawLine(position(x, y), position(x+1, y),Color.white,100f);
+
+
             }
 
         }
     }
     private Vector3 position(int x,int y)
     {
-        return new Vector3(x,y)*CellSize;
+        return new Vector3(x,y)*CellSize+ parentPosition;
     }
     private void GetWorldposition(Vector3 position,out int x, out int y) 
     {
@@ -49,8 +59,8 @@ public class Grid
         SetValue(x, y, value);
     }
 
-
-    public static TextMesh CreateWorldText(Transform parent, string text, Vector3 localPosition, int fontSize, Color color, TextAnchor textAnchor, TextAlignment textAlignment, int sortingOrder)
+    
+    public static TextMesh CreateWorldText(Transform parent, string text, Vector3 localPosition, int fontSize, Color color, TextAnchor textAnchor,TextAlignment textAlignment, int sortingOrder)
     {
         GameObject gameObject = new GameObject("World_Text", typeof(TextMesh));
         Transform transform = gameObject.transform;
--- Assets/Scripts/gridtest.cs
+++ Assets/Scripts/gridtest.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class gridtest : MonoBehaviour
 {
-    // Start is called before the first frame update
+    new Vector3 mousePosition;
     void Start()
     {
-        Grid grid = new Grid(20, 10);
+        Grid grid = new Grid(20, 10,10f,transform);
     }
 
-    
+   //public void MousePosition() 
+    //{
+      //  mousePosition = mainCam.ScreenToWorldPoint(Mouse.current.position);
+    //}
+
 }
